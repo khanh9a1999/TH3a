8a53e85db34c7c2c2f51fd776328663a4ea261d5
khanh9a1999 <70497384+khanh9a1999@users.noreply.github.com>
2020-09-21 22:08:13 +0700

    update

---
+++ Bai1/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PagesComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Bai1/src/app/pages/pages.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-pages',
+  templateUrl: './pages.component.html',
+  styleUrls: ['./pages.component.css']
+})
+export class PagesComponent implements OnInit {
+
+  ngOnInit(): void {
+  }
+
+}
